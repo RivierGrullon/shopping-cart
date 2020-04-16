c4537b4fb4b91f009a63486fac9faff7fd8d7853
Jose Esteban <josegg1897@gmail.com>
2020-04-16 09:52:37 -0400

    Move file

---
+++ src/components/CartProduct.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const CartProduct = ({product, UpdateAmount}) => {
+    const {id,name,seller,image,price,mount} = product
+    return (
+        <div className="Product-container" key={id}>
+            <div className="Product-image">
+                <img src={image} alt={name} />
+            </div>
+            <div className="Product-info">
+                <p id="name">{name}</p>
+                <p id="seller">{seller}</p>
+            </div>
+            <div className="Product-mount">
+                <input type="image" src="/images/add.svg" alt="add" onClick={UpdateAmount(id, 'add')}/>
+                <p>{mount}</p>
+                <button onClick={UpdateAmount(id, 'sub')}>-</button>
+            </div>
+            <div className="Product-price">
+                <p>{(price * mount).toFixed(2)}</p>
+            </div>
+        </div>
+    )
+}
+
+export default CartProduct;
